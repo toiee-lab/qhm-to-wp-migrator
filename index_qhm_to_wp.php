d7561d7bb4717a4c9e2d1c325bd752a3f19d50b4
T. Kameda <kame@toiee.jp>
2017-10-17 23:41:33 +0900

    301リダイレクトを正しく行うように修正
    Index_qhm.phpへのアクセスを強制的にリダイレクトするためのファイルを修正しました。

--- index_qhm_to_wp.php
+++ index_qhm_to_wp.php
@@ -2,8 +2,10 @@
 	
 // THIS_IS_QHM_TO_WP_REDIRECTION
 
-$url = dirname($_SERVER["REQUEST_URI"]).'/'.$_SERVER['QUERY_STRING'];
 
+$url = (empty($_SERVER['HTTPS']) ? 'http://' : 'https://').$_SERVER['HTTP_HOST']. dirname($_SERVER["REQUEST_URI"]).'/'.$_SERVER['QUERY_STRING'];
+
+header( "HTTP/1.1 301 Moved Permanently" );
 header("Location: $url");
 exit;
 
